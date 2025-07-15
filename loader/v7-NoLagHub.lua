4222547bbb53d959bddf17c692e630c0a4e8107c
NoLagHubV7 <nolaghub7@gmail.com>
2025-07-15 12:44:30 +0800

    Delete loader/v7-NoLagHub.lua

--- loader/v7-NoLagHub.lua
+++
@@ -1,2 +0,0 @@
-loadstring(game:HttpGet("https://pastefy.app/1h6Rxw0F/raw"))()
-loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Loader/LoaderV1.lua"))()
