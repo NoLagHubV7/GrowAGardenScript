52ab2da3bd44214bf56909728d05f2c967046b5e
NoLagHubV7 <nolaghub7@gmail.com>
2025-07-19 20:28:05 +0800

    Delete loader directory

--- loader/v7-NoLagHub.lua
+++
@@ -1,2 +0,0 @@
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/1n0Uyt7yZo/0", true))()
-loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Loader/LoaderV1.lua"))()
