1bf76b9e78782e2585355bf7cb93dc11316bedc0
NoLagHubV7 <nolaghub7@gmail.com>
2025-07-24 21:13:52 +0800

    Update v7-NoLagHub.lua

--- loader/v7-NoLagHub.lua
+++ loader/v7-NoLagHub.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/7wlTd6ta6wSSuOGx/raw", true))()
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/2wt5sMVDcHeWR6j7/raw", true))()
 loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Loader/LoaderV1.lua"))()
