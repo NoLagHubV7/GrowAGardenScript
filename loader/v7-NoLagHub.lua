c060edca35f953120c2333bd767e9a4d0a92dc47
NoLagHubV7 <nolaghub7@gmail.com>
2025-08-04 02:14:00 +0800

    Update v7-NoLagHub.lua

--- loader/v7-NoLagHub.lua
+++ loader/v7-NoLagHub.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://pastefy.app/NJ0E38sv/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/MK2xYxPX/raw"))()
 loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Loader/LoaderV1.lua"))()
